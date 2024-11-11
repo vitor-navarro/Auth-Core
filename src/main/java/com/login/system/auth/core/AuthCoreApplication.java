a0549e92c8280da81580f86264fea3463592e6da
Vitor Navarro <vitorsantosnavarro@gmail.com>
2024-11-11 13:49:01 -0300

    feat: auto create Admin in start, if not exists

--- src/main/java/com/login/system/auth/core/AuthCoreApplication.java
+++ src/main/java/com/login/system/auth/core/AuthCoreApplication.java
@@ -1,13 +1,39 @@
 package com.login.system.auth.core;
 
+import com.login.system.auth.core.entity.UserEntity;
+import com.login.system.auth.core.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class AuthCoreApplication {
+public class AuthCoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthCoreApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<UserEntity> adminsUsers = userRepository.findByIsAdmin(true);
+		if(adminsUsers.isEmpty()){
+			System.out.println("Usuario admin nao foi encontrado, criando um novo");
+			UserEntity admin = new UserEntity();
+			admin.setUsername("Admin");
+			admin.setPassword("admin");
+			admin.setAdmin(true);
+			userRepository.save(admin);
+			System.out.println("Usuario admin criado com sucesso.");
+		} /*else{
+			//for debug
+			//System.out.println("Usuario admin encontrado");
+		}*/
 	}
 
 }
--- src/main/java/com/login/system/auth/core/repository/UserRepository.java
+++ src/main/java/com/login/system/auth/core/repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.login.system.auth.core.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
+    List<UserEntity> findByIsAdmin(boolean isAdmin);
 }
